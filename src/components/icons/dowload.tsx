c6734058ef133677e0ce6b5ff5a2af8d625d140c
santiago <santiagoricopassop@gmail.com>
2024-01-19 20:08:06 -0500

    📕  Was Update Icons

--- src/components/icons/dowload.tsx
+++ src/components/icons/dowload.tsx
@@ -1,10 +1,10 @@
-const MapPin = () => {
+const Dowload = () => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      className="icon icon-tabler icon-tabler-map-pin"
-      width="24"
-      height="24"
+      className="icon icon-tabler icon-tabler-download"
+      width="19"
+      height="19"
       viewBox="0 0 24 24"
       stroke-width="2"
       stroke="currentColor"
@@ -13,10 +13,10 @@ const MapPin = () => {
       stroke-linejoin="round"
     >
       <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-      <path d="M9 11a3 3 0 1 0 6 0a3 3 0 0 0 -6 0" />
-      <path d="M17.657 16.657l-4.243 4.243a2 2 0 0 1 -2.827 0l-4.244 -4.243a8 8 0 1 1 11.314 0z" />
+      <path d="M4 17v2a2 2 0 0 0 2 2h12a2 2 0 0 0 2 -2v-2" />
+      <path d="M7 11l5 5l5 -5" />
+      <path d="M12 4l0 12" />
     </svg>
   )
 }
-
-export default MapPin
+export default Dowload
